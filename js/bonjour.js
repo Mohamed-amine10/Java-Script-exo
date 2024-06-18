3f2df85f8a41fa64557d0b2f86cd9e5e628b8af9
satir <mohamedsatir6@gmail.com>
2024-06-18 09:38:37 +0200

    Java Script exo

---
+++ js/bonjour.js
@@ -0,0 +1,11 @@
+window.alert("BIENVENUE!!")
+let nom;
+let prenom;
+
+nom = prompt("Question: quel est to nom?", "Nom" )
+console.log(nom);
+
+prenom = prompt("Question: quel est ton prenom ?", "Prenom");
+console.log(prenom);
+
+alert( "Vous etes Monsieur... " + nom + " " + prenom)
